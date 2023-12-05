856e55690dec8485bc08a50073014524af8e26e2
winniecat777 <winniecat777@gmail.com>
2023-12-05 16:14:18 +0800

    add url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,4 @@
 const api_path ="winnie";
 const token = "0aETcFewwMUUvJLKKzPaNlBNHqz1";
+const url = 'https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}';
+const URL = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${api_path}/';
