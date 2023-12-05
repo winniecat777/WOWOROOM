e01e3ae672b71f8a7fe5433c8258c4a64988f55e
winniecat777 <winniecat777@gmail.com>
2023-12-05 17:45:15 +0800

    add ProductList

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 const productList = document.querySelector('.productWrap');
 
+let productData = [];
 
 function init(){
-  // getProductList();
+  getProductList();
 }
 init();
 
@@ -11,6 +12,24 @@ function getProductList(){
 	.then((res) => {
 		console.log(res);
 		productData = response.data.products;
-		// console.log(productData);	 
+		// console.log(productData);
+		renderProductList();	 
 	})
+}
+
+function renderProductList(){
+	let str = "";
+	productData.forEach((item) => {
+		str += `
+		<li class="productCard">
+		<h4 class="productType">新品</h4>
+		<img src="${item.images}" alt="">
+		<a href="#" class="addCardBtn" data-id="${item.id}">加入購物車</a>
+		<h3>${item.title}</h3>
+		<del class="originPrice">NT$${toThousands(item.origin_price)}</del>
+		<p class="nowPrice">NT$${toThousands(item.price)}</p>
+		</li>
+		`
+	})
+	getProductList.innerHTML = str;
 }
